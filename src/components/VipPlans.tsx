952e025f63d4937a7e68ebc9764226c88fa0a036
tomi <tomi@tomi-2.local>
2025-04-06 19:46:52 +0900

    删除未使用的 useEffect 导入

--- src/components/VipPlans.tsx
+++ src/components/VipPlans.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { translations } from '../i18n/translations';
 import { useLanguage } from '../contexts/LanguageContext';
